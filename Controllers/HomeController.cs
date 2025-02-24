023526188c087b17e473deb87f67db6f54d41ff6
rutika005 <134037361+rutika005@users.noreply.github.com>
2025-02-24 22:05:40 +0530

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Aesthetica.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
