abb831d031c77247989773496f9e24bbc08b5777
rutika005 <134037361+rutika005@users.noreply.github.com>
2025-03-10 20:01:00 +0530

    changes in sppdbcontext file

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aesthetica.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ContactModel> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=aesthetica;User Id=root;Password=;",
+                new MySqlServerVersion(new Version(8, 0, 23)));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactModel>().ToTable("Contacts"); // Maps ContactModel to Contacts table
+        }
+    }
+}
--- Models/ContactModel.cs
+++ Models/ContactModel.cs
@@ -4,6 +4,9 @@ namespace Aesthetica.Models
 {
     public class ContactModel
     {
+        [Key]
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
         [StringLength(50, ErrorMessage = "Name can't be longer than 50 characters")]
         public string Name { get; set; }
