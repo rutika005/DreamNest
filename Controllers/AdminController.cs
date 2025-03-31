aadd7a96b3c55a508a49089abd9165f84c26fc82
Shruti Chavda <schavda684@rku.ac.in>
2025-04-01 00:11:58 +0530

    Blog page done

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -16,7 +16,13 @@ namespace Aesthetica.Controllers
 
         public IActionResult Blog()
         {
-            return View();
+            List<Models.BlogPost> blogPosts = new List<Models.BlogPost>
+        {
+            new Models.BlogPost { Id = 1, Title = "Modern Interior Design Trends 2025", Category = "Interior Design", Author = "Sarah Johnson", AuthorImage = "/images/p1.png", Status = "Published", Date = new DateTime(2025, 1, 15), Thumbnail = "/images/blog1.png" },
+            new Models.BlogPost { Id = 2, Title = "Luxury Bedroom Designs", Category = "Decoration", Author = "Mike Wilson", AuthorImage = "/images/p2.png", Status = "Draft", Date = new DateTime(2025, 1, 12), Thumbnail = "/images/blog2.png" }
+        };
+
+            return View(blogPosts);
         }
 
         public IActionResult Budget()
---
+++ Models/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace Aesthetica.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+        public string AuthorImage { get; set; } // Profile Image URL
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+        public string Thumbnail { get; set; } // Blog Post Image
+    }
+
+}
